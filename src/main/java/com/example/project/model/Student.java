6433f0c0ca8c38e9b01bda20c4b766162486d236
sushmita-sing <94803174+sushmita-sing@users.noreply.github.com>
2022-01-14 13:13:30 +0530

    Added mainclass

---
+++ src/main/java/com/example/project/model/Student.java
@@ -0,0 +1,14 @@
+package com.example.project.model;
+
+public class Student {
+
+    private Long id;
+    private String name;
+    private String branch;
+
+    public Student(Long id, String name, String branch) {
+        this.id = id;
+        this.name = name;
+        this.branch = branch;
+    }
+}
